e5414d922ea4ab327769154bd357a29bc31c179b
vishalmet <vishalmet003@gmail.com>
2023-04-05 17:11:26 +0530

    tailwind commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
+  <div className="black">
+     <h1 className='bg-black text-white'>Hello world</h1>
     </div>
   );
 }
